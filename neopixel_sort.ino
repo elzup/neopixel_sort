5ad125404cbaac144ff363a1d0a2967f440834b1
elzup <guild0105@gmail.com>
2017-05-27 00:36:41 +0900

    Create bitnicsort

--- neopixel_sort.ino
+++ neopixel_sort.ino
@@ -73,6 +73,50 @@ void selectionSort() {
     destory();
 }
 
+void bitnicSort() {
+    initialize();
+    set_delay(1, 1);
+    shuffle();
+    revBitnic(0, NUMPIXELS - 1, true);
+    pixels.show();
+    destory();
+}
+
+void checkSwap(v1, v2, asc = true) {
+    if ((m[v1] > m[v2]) != asc) {
+        noswap(v1, v2)
+        return;
+    }
+    swap(v1, v2)
+}
+
+void cascade(left, right, asc) {
+    if (left + 1 == right) {
+        checkSwap(left, right, asc);
+        return;
+    }
+    int half = (right - left + 1) / 2;
+    int middle = left + half;
+    for (let i = 0; i < half; ++i) {
+        checkSwap(left + i, middle + i, asc);
+    };
+    cascade(left, middle - 1, asc);
+    cascade(middle, right, asc);
+}
+
+void revBitnic(left, right, asc) {
+    int half = (right - left + 1) / 2;
+    int middle = left + half;
+    if (left + 1 == right) {
+        checkSwap(left, right, asc);
+        return;
+    }
+    revBitnic(left, middle - 1, true);
+    revBitnic(middle, right, false);
+    cascade(left, right, asc);
+}
+
+
 void insertionSort() {
     initialize();
     set_delay(1, 20);
